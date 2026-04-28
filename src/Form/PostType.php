3dbb7651be71546787f9354685e3f06907717113
Azad1981 <azadtanz9gmail.com>
2026-04-28 09:35:16 +0200

    creation des crud post et category

---
+++ src/Form/PostType.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Post;
+use App\Entity\user;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class PostType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('title')
+            ->add('content')
+            ->add('published', null, [
+                'widget' => 'single_text'
+            ])
+            ->add('isFeatured')
+            ->add('author', EntityType::class, [
+                'class' => user::class,
+                'choice_label' => 'id',
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Post::class,
+        ]);
+    }
+}
